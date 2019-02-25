65375dd1a865feaa7a3af5ac08490a8af6d55ec2
nguest <nguest@workwithopal.com>
2019-02-24 16:20:53 -0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nano-drum
+8 bit ARDUINO NANO drum machine inspired by Jan Ostman but adding playability to existing sequencer
+TODO: 
+- Record mode
+- Possible dual 8-bit output
+- SD card for more samples
+- Sample timestretching?
+
--- nano-drum.ino
+++ nano-drum.ino
@@ -1,8 +1,4 @@
-// O2 Minipops rhythm box (c) DSP Synthesizers 2016
-// Free for non commercial use
-
-// http://janostman.wordpress.com
-
+// 8 bit ARDUINO NANO drum machine inspired by Jan Ostman but adding playability to existing sequencer
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -462,7 +458,7 @@ void setup() {
   ADCSRA &= ~PS_128;  // remove bits set by Arduino library
   // Choose prescaler PS_128.
   ADCSRA |= PS_128;
-  ADMUX = 64;
+  ADMUX = 64; 
   sbi(ADCSRA, ADSC);
 
 
